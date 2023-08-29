0aea356c94bf5c813bb3e7c412af7402283f523f
Jonathan Sánchez <jonulisanpar@gmail.com>
2023-08-28 21:55:45 -0600

    TryCatch block in getRecentMManga Controll

--- controllers/otome.controller.ts
+++ controllers/otome.controller.ts
@@ -10,39 +10,44 @@ import { limpiarNombre } from "../helpers/limpiarDatos";
 axios.defaults.withCredentials = true;
 
 export const getRecentManga = async(req: Request, res: Response) => {
-  const URL = process.env.URL_MANGA_OTOME!
-  console.log(URL);
-  console.log(process.env.URL_MANGA_OTOME);
-  const {status, data} = await axios.get(URL);
-  
-  if (status == 200) {
-    const $ = cheerio.load(data);
-    const containerManga = $('#top_weekly');
-    const listM: Manga[] = [];
+  try {
+    const URL = process.env.URL_MANGA_OTOME!
+    console.log(URL);
+    console.log(process.env.URL_MANGA_OTOME);
+    const { status, data } = await axios.get(URL);
 
-    containerManga.children().each((i, el) => {
+    if (status == 200) {
+      const $ = cheerio.load(data);
+      const containerManga = $('#top_weekly');
+      const listM: Manga[] = [];
 
-      const cover = $(el).find('img.content-thumbnail-cover').attr()!.src
-      const { title, href } = $(el).find('.content-title a').attr()!;
-      const mid = href.split('/')[4];
-      listM.push({
-        title,
-        mid,
-        cover,
-        infoPage: href
+      containerManga.children().each((i, el) => {
+
+        const cover = $(el).find('img.content-thumbnail-cover').attr()!.src
+        const { title, href } = $(el).find('.content-title a').attr()!;
+        const mid = href.split('/')[4];
+        listM.push({
+          title,
+          mid,
+          cover,
+          infoPage: href
+        })
+      });
+
+      res.status(200).json({
+        ok: true,
+        result: listM
       })
-    });
-
-    res.status(200).json({
-      ok: true,
-      result: listM
-    })
-  } else {
-    res.status(500).json({
-      ok: false,
-      result: `Error con consulta en ${URL}`
-    })
+    } else {
+      res.status(500).json({
+        ok: false,
+        result: `Error con consulta en ${URL}`
+      })
+    }
+  } catch (error) {
+    console.log(error)
   }
+  
 }
 
 export const getDetailManga = async(req: Request, res: Response) => {
